00bab2131b3058b7a494dfaefaad819bc02bb4d4
=SeanMonteiro <=monteiro.sean18@gmail.com>
2026-05-13 22:58:00 -0700

    Fixed Raw Input Parser for raw ingestion reliability

--- app/utils/raw_job_parser.py
+++ app/utils/raw_job_parser.py
@@ -1,8 +1,41 @@
+MAX_TITLE_LENGHT = 80
+MAX_COMPANY_LENTH = 100
 def parse_raw_job_text(text:str):
+    if not text or not text.strip():
+        raise ValueError("Empty job description")
+    
     lines = [line.strip() for line in text.splitlines() if line.strip()]
-    data = {
-        "title": lines[0] if len(lines) > 0 else "Unknown",
-        "company" : lines[1] if len(lines) > 0 else "Unknown",
-        "description": "\n".join(lines[2:]) if len(lines) > 2 else ""
+    
+    line1 = lines[0] if len(lines) > 0 else None
+    # print(f"Lines [0]: {lines[0]}\n")
+    
+    line2 = lines[1] if len(lines) > 1 else "Unknown"
+    # print(f"Lines [1]: {lines[1]}\n")
+    
+    description_lines = lines[2:]
+
+    # for index, line in enumerate(lines[2:], start=2):
+    #     print(f"Line [{index}]: {line}")
+
+    # Reconstructing original description
+
+    ## Validate if truly company name or just another description text
+    company = line2 if len(line2) <= MAX_COMPANY_LENTH else "Unknown"
+    # description_lines.insert(0, company)
+    if company =="Unknown":
+        description_lines.insert(0,line2)
+
+    # # Validate if try title name or just another description text
+    title = line1 if len(line1) <= MAX_TITLE_LENGHT else "Unknown"
+    # description_lines.insert(0, title)
+    if title == "Unknown":
+        description_lines.insert(0,line1)
+
+    description = "\n".join(description_lines)
+
+    # print("#### --------- PRINTING OBJECT ------------ ####")
+    return {
+        "title": title,
+        "company" : company,
+        "description": description
     }
-    return data
